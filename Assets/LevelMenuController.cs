4ceca2cb1f8c1846ee782631943dd89ace44dd6f
kuihsan <ihsannudin266@gmail.com>
2026-04-21 13:20:13 +0800

    Add TransitionManager scene transitions
    Replace direct SceneManager.LoadScene calls with TransitionManager.Instance.LoadLevel (0.5s) in StartMenuController and LevelMenuController and add MaskTransitions namespace. Add TransitionManager prefab instances to Level 1/2/3, LevelScene and NewStartScene (serialized prefab modifications). Update TransitionManager.prefab serialized fields (canvas/rect/mask properties and other serialization tweaks). Also adjust an Animator state node position in AC_Player.controller.

--- Assets/LevelMenuController.cs
+++ Assets/LevelMenuController.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using MaskTransitions;
 
 public class LevelMenuController : MonoBehaviour
 {
    public void OnLevel1Click()
    {
-	   SceneManager.LoadScene("SampleScene");
-   }
+		TransitionManager.Instance.LoadLevel("Level 1", 0.5f);
+        //SceneManager.LoadScene("Level 1");
+    }
 
    public void OnLevel2Click()
    {
-	    SceneManager.LoadScene("SampleScene2");
-   }
+        TransitionManager.Instance.LoadLevel("Level 2", 0.5f);
+        //SceneManager.LoadScene("Level 2");
+    }
 
    public void OnLevel3Click()
    {
-	   SceneManager.LoadScene("SampleScene3");
-   }
+		TransitionManager.Instance.LoadLevel("Level 3", 0.5f);
+        //SceneManager.LoadScene("Level 3");
+    }
 
 }
--- Assets/StartMenuController.cs
+++ Assets/StartMenuController.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using MaskTransitions;
 
 public class StartMenuController : MonoBehaviour
 {
    public void OnStartClick()
    {
-	   SceneManager.LoadScene("LevelScene");
+		TransitionManager.Instance.LoadLevel("LevelScene", 0.5f);
    }
 
    public void OnExitClick()
