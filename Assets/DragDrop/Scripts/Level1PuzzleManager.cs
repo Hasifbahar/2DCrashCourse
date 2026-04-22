45a3e0d97b5b22b48de58c52cce4ad571f6c9cbc
kuihsan <ihsannudin266@gmail.com>
2026-04-22 21:33:53 +0800

    Add Rainbow Man character assets and animations
    Bulk import of Rainbow Man character assets: dozens of Aseprite files, PNGs, animation clips and .meta files for various states (idle, run, jump, attacks, climb, etc.). Added Idle.anim and PlayScene animator controller; updated PlayerImage.controller. Also added several Slime and Win animations and applied changes to Level1PuzzleManager.cs and the Puzzle 1.unity scene to integrate the new assets.

--- Assets/DragDrop/Scripts/Level1PuzzleManager.cs
+++ Assets/DragDrop/Scripts/Level1PuzzleManager.cs
@@ -1,43 +1,104 @@
+using System.Collections;
 using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
     public ItemSlot[] slots;
+
     [SerializeField] private GameObject winUI;
     [SerializeField] private GameObject tryUI;
+    [SerializeField] private Animator workerAnimator;
 
     private void Start()
     {
         winUI.SetActive(false);
         tryUI.SetActive(false);
+
+        SetIdle(true);
     }
     public void CheckAnswer()
     {
+        StartCoroutine(CheckAndPlay());
+    }
+    IEnumerator CheckAndPlay()
+    {
         winUI.SetActive(false);
         tryUI.SetActive(false);
-        foreach (ItemSlot slot in slots)
+
+        SetIdle(true);
+        yield return new WaitForSeconds(0.2f); // small pause before start
+
+        for (int i = 0; i < slots.Length; i++)
         {
-            if (!slot.IsCorrect())
+            if (slots[i].IsCorrect())
             {
-                WrongAnswer();
-                return;
+                // Leave idle  play answer animation
+                SetIdle(false);
+                ResetAll();
+
+                PlayCorrectAnimation(i);
+                yield return new WaitForSeconds(0.8f);
+
+                // Back to idle briefly
+                ResetAll();
+                SetIdle(true);
+                yield return new WaitForSeconds(0.3f);
+            }
+            else
+            {
+                SetIdle(false);
+                ResetAll();
+
+                workerAnimator.SetBool("dead", true);
+                yield return new WaitForSeconds(3f);
+
+                tryUI.SetActive(true);
+
+                ResetAll();
+                SetIdle(true);
+                yield break;
             }
         }
 
-        CorrectAnswer();
-    }
+        // All correct  win
+        SetIdle(false);
+        ResetAll();
 
-    void CorrectAnswer()
-    {
-        Debug.Log("Correct!");
-        // Worker moves
+        workerAnimator.SetBool("win", true);
+        yield return new WaitForSeconds(1f);
+
         winUI.SetActive(true);
+
+        ResetAll();
+        SetIdle(true);
     }
 
-    void WrongAnswer()
+    void PlayCorrectAnimation(int index)
     {
-        Debug.Log("Type mismatch detected.");
-        // Worker freeze animation
-        tryUI.SetActive(true);
+        switch (index)
+        {
+            case 0:
+                workerAnimator.SetBool("ans1", true);
+                break;
+            case 1:
+                workerAnimator.SetBool("ans2", true);
+                break;
+            case 2:
+                workerAnimator.SetBool("ans3", true);
+                break;
+        }
+    }
+    void ResetAll()
+    {
+        workerAnimator.SetBool("ans1", false);
+        workerAnimator.SetBool("ans2", false);
+        workerAnimator.SetBool("ans3", false);
+        workerAnimator.SetBool("dead", false);
+        workerAnimator.SetBool("win", false);
+    }
+
+    void SetIdle(bool state)
+    {
+        workerAnimator.SetBool("Idle", state);
     }
 }
