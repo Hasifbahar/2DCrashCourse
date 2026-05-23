e265feb8c0af237409cc877c9ea1b7b8ca3f3e73
Hasifbahar <shimisif@gmail.com>
2026-05-24 00:07:48 +0800

    update

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,14 +6,23 @@ public class Enemy : MonoBehaviour
 {
     Animator animator;
     GameUI gameUI;
+    
     public float Health {
         set {
             health = value;
             if(health <= 0) {
                 Defeated();
-                gameUI.score += 1;
-                gameUI.UpdateScore();
-                gameUI.ShowTutorial();
+                
+                // Safety check: Prevent errors if GameUI isn't in the scene yet
+                if (gameUI != null) {
+                    gameUI.score += 1;
+                    gameUI.UpdateScore();
+                    gameUI.ShowTutorial();
+                }
+            }
+            else {
+                // ADDED: This plays the hit animation if health is still > 0
+                animator.SetTrigger("Hit"); 
             }
         }
         get {
@@ -21,7 +30,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public float health = 1;
+    public float health = 3; // Increased starting health so it survives a hit!
 
     public void Start() {
         animator = GetComponent<Animator>();
@@ -32,7 +41,8 @@ public class Enemy : MonoBehaviour
         animator.SetTrigger("Defeated");
     }
 
-    public void RemoveEnemey() {
+    // (Make sure you call this from an Animation Event at the end of the death animation!)
+    public void RemoveEnemy() { // (Fixed spelling from RemoveEnemey)
         Destroy(gameObject);
     }
 }
